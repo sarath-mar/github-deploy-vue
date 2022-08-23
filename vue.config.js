9fa34aedc4bf0176a68c6fb2236970f70344d4cd
LAPTOP-EHDI9A5M\sarat <sarath@arminotech.in>
2022-08-23 19:53:19 +0530

    footer added nd mobile view

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/github-deploy-vue/'
+//     : '/'
+// }
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/github-deploy-vue/'
-    : '/'
+  publicPath: '/'
 }
