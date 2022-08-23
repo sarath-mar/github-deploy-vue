421137e7ed8c982f01eba552a5ad12981dd5e056
LAPTOP-EHDI9A5M\sarat <sarath@arminotech.in>
2022-08-23 22:18:01 +0530

    t

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:sarath-mar/deploy-to-custom-domain.git main:gh-pages
+git push -f git@github.com:sarath-mar/deploy-to-custom-domain.git master:gh-pages
 
 cd -
