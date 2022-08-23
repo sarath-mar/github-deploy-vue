f1e0b9178f93b24c535c44d26fd9edf56f3bd62e
LAPTOP-EHDI9A5M\sarat <sarath@arminotech.in>
2022-08-23 22:08:57 +0530

    h1 tag added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-     <h1>helooi</h1>
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+   <h1>new deployment script added</h1>
   </div>
 </template>
 
